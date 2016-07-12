525b46935d50e223c228bbf48eae842cb4866cec
Sergey Shisholik <shisholik@realtimeboard.com>
2016-07-12 15:21:33 +0500

    Added information about instances for some events

--- fields.js
+++ fields.js
@@ -1,116 +1,163 @@
 var _ = require('lodash');
+var AWS = require('aws-sdk');
+var Q = require('q');
+var ec2 = new AWS.EC2();
+
+function describeInstances(instancesIds, region) {
+	var deferred = Q.defer();
+	var params = {
+		InstanceIds: instancesIds
+	};
+
+	AWS.config.update({region: region});
+
+	ec2.describeInstances(params, function (err, data) {
+		var fields = [
+			{
+				title: instancesIds.length > 1 ? 'Instances' : 'Instance',
+				value: _.join(instancesIds, ', '),
+				short: true
+			}
+		];
+		var additionalFields = [];
+
+		if (err) {
+			deferred.reject(err)
+		} else {
+			if (data.Reservations[0]) {
+				var awsInstances = data.Reservations[0].Instances;
+				var instanceTypes = _.uniq(_.map(awsInstances, function (item) {
+					return item.InstanceType;
+				}));
+				var privateIpAddresses = _.map(awsInstances, function (item) {
+					return item.PublicIpAddress;
+				});
+				var names = _.map(awsInstances, function (item) {
+					return item.Tags.filter(function (obj) { return obj.Key == "Name" })[0].Value;
+				});
+
+				additionalFields = [
+					{
+						title: instanceTypes.length > 1 ? 'Instance Types' : 'Instance Type',
+						value: _.join(instanceTypes, ', '),
+						short: true
+					},
+					{
+						title: instancesIds.length > 1 ? 'Ips' : 'Ip',
+						value: _.join(privateIpAddresses, ', '),
+						short: true
+					},
+					{
+						title: instancesIds.length > 1 ? 'Instances Names' : 'Instance Name',
+						value: _.join(names, ', '),
+						short: true
+					}
+				]
+			}
+
+		}
+		deferred.resolve(_.concat(fields, additionalFields))
+	});
+
+	return deferred.promise;
+}
 
 module.exports = {
-  ConsoleLogin: function(event) {
-    return [
-      {
-        title: 'Source IP',
-        value: event.sourceIPAddress,
-        short: true
-      },
-      {
-        title: 'MFA Used',
-        value: event.additionalEventData.MFAUsed,
-        short: true
-      }
-    ]
-  },
-  RunInstances: function(event) {
-    var items = event.responseElements.instancesSet.items;
-    var instances = _.map(items, function(item) {
-      return item.instanceId;
-    });
-    var instanceTypes = _.uniq(_.map(items, function(item) {
-      return item.instanceType;
-    }));
-    var privateIpAddresses = _.map(items, function(item) {
-      return item.privateDnsName;
-    });
+	ConsoleLogin: function (event) {
+		return Q.resolve([
+			{
+				title: 'Source IP',
+				value: event.sourceIPAddress,
+				short: true
+			},
+			{
+				title: 'MFA Used',
+				value: event.additionalEventData.MFAUsed,
+				short: true
+			}
+		])
+	},
+	RunInstances: function (event) {
+		var items = event.responseElements.instancesSet.items;
+		var instances = _.map(items, function (item) {
+			return item.instanceId;
+		});
 
-    return [
-      {
-        title: instanceTypes.length > 1 ? 'Instance Types' : 'Instance Type',
-        value: _.join(instanceTypes, ', '),
-        short: true
-      },
-      {
-        title: instances.length > 1 ? 'Instances' : 'Instance',
-        value: _.join(instances, ', '),
-        short: true
-      },
-      {
-        title: instances.length > 1 ? 'Hostnames' : 'Hostname',
-        value: _.join(privateIpAddresses, ', '),
-        short: true
-      }
-    ]
-  },
-  SetDesiredCapacity: function(event) {
-    return [
-      {
-        title: 'Group Name',
-        value: event.requestParameters.autoScalingGroupName,
-        short: true
-      },
-      {
-        title: 'Desired Capacity',
-        value: event.requestParameters.desiredCapacity,
-        short: true
-      }
-    ]
-  },
-  TerminateInstances: function(event) {
-    var instances = _.map(event.requestParameters.instancesSet.items, function(item) {
-      return item.instanceId;
-    });
+		return describeInstances(instances, event.awsRegion)
+	},
+	StartInstances: function (event) {
+		var items = event.responseElements.instancesSet.items;
+		var instances = _.map(items, function (item) {
+			return item.instanceId;
+		});
 
-    return [
-      {
-        title: instances.length > 1 ? 'Instances' : 'Instance',
-        value: _.join(instances, ', '),
-        short: true
-      }
-    ]
-  },
-  UpdateFunctionCode20150331: function(event) {
-    return [
-      {
-        title: 'Function',
-        value: event.responseElements.functionName,
-        short: true
-      }
-    ]
-  },
-  UpdateService: function(event) {
-    var fields = [
-      {
-        title: 'Cluster',
-        value: event.requestParameters.cluster,
-        short: true
-      },
-      {
-        title: 'Service',
-        value: event.requestParameters.service,
-        short: true
-      }
-    ]
+		return describeInstances(instances, event.awsRegion)
+	},
+	StopInstances: function (event) {
+		var items = event.responseElements.instancesSet.items;
+		var instances = _.map(items, function (item) {
+			return item.instanceId;
+		});
 
-    if (event.requestParameters.taskDefinition) {
-      fields.push({
-        title: 'Task Definition',
-        value: event.requestParameters.taskDefinition,
-        short: true
-      });
-    }
+		return describeInstances(instances, event.awsRegion)
+	},
+	TerminateInstances: function (event) {
+		var instances = _.map(event.requestParameters.instancesSet.items, function (item) {
+			return item.instanceId;
+		});
 
-    if (event.requestParameters.desiredCount) {
-      fields.push({
-        title: 'Desired Count',
-        value: event.requestParameters.desiredCount,
-        short: true
-      });
-    }
+		return Q.resolve([
+			{
+				title: instances.length > 1 ? 'Instances' : 'Instance',
+				value: _.join(instances, ', '),
+				short: true
+			}
+		])
+	},
+	SetDesiredCapacity: function (event) {
+		return Q.resolve([
+			{
+				title: 'Group Name',
+				value: event.requestParameters.autoScalingGroupName,
+				short: true
+			},
+			{
+				title: 'Desired Capacity',
+				value: event.requestParameters.desiredCapacity,
+				short: true
+			}
+		])
+	},
+	UpdateService: function (event) {
+		var fields = [
+			{
+				title: 'Cluster',
+				value: event.requestParameters.cluster,
+				short: true
+			},
+			{
+				title: 'Service',
+				value: event.requestParameters.service,
+				short: true
+			}
+		];
 
-    return fields;
-  }
-}
+		if (event.requestParameters.taskDefinition) {
+			fields.push({
+				title: 'Task Definition',
+				value: event.requestParameters.taskDefinition,
+				short: true
+			});
+		}
+
+		if (event.requestParameters.desiredCount) {
+			fields.push({
+				title: 'Desired Count',
+				value: event.requestParameters.desiredCount,
+				short: true
+			});
+		}
+
+		return Q.resolve(fields);
+	}
+};
--- slacktrail.js
+++ slacktrail.js
@@ -8,124 +8,148 @@ var Dotenv = require('dotenv');
 var Fields = require('./fields');
 
 function read(input) {
-  var deferred = Q.defer();
-  var unzip = Zlib.createGunzip();
-  var payload = new Buffer(input, 'base64');
+	var deferred = Q.defer();
+	var unzip = Zlib.createGunzip();
+	var payload = new Buffer(input, 'base64');
 
-  Zlib.gunzip(payload, function(error, result) {
-    if (!error) {
-      logMessage = JSON.parse(result.toString('ascii'));
-      events = _.map(logMessage.logEvents, function(logEvent) {
-        return JSON.parse(logEvent.message);
-      });
+	Zlib.gunzip(payload, function (error, result) {
+		if (!error) {
+			logMessage = JSON.parse(result.toString('ascii'));
+			events = _.map(logMessage.logEvents, function (logEvent) {
+				return JSON.parse(logEvent.message);
+			});
 
-      deferred.resolve(events);
-    } else {
-      deferred.reject(error);
-    }
-  });
+			deferred.resolve(events);
+		} else {
+			deferred.reject(error);
+		}
+	});
 
-  return deferred.promise;
+	return deferred.promise;
 }
 
 function filter(events) {
-  var deferred = Q.defer();
-  var ignoreEvents = process.env.IGNORE_EVENTS.split(',')
 
-  filteredEvents = _.filter(_.compact(events))
+	var deferred = Q.defer();
+	var ignoreEvents = process.env.IGNORE_EVENTS.split(',')
 
-  filteredEvents = _.reject(filteredEvents, function(event) {
-    return _.some(ignoreEvents, function(ignoreEvent) {
-      return event.eventName.indexOf(ignoreEvent) === 0
-    })
-  });
+	filteredEvents = _.filter(_.compact(events))
 
-  if (filteredEvents.length > 0) {
-    deferred.resolve(filteredEvents);
-  } else {
-    deferred.reject('No events.');
-  }
+	filteredEvents = _.reject(filteredEvents, function (event) {
+		return _.some(ignoreEvents, function (ignoreEvent) {
+			return event.eventName.indexOf(ignoreEvent) === 0
+		})
+	});
 
-  return deferred.promise;
+	if (filteredEvents.length > 0) {
+		deferred.resolve(filteredEvents);
+	} else {
+		deferred.reject('No events.');
+	}
+
+	return deferred.promise;
 }
 
 function announce(events) {
-  var deferred = Q.defer();
-  var slack = new Slack();
 
-  var attachments = _.map(events, function(event) {
-    var user;
-    var fields;
+	var attachments = _.map(events, function (event) {
+			var deferred = Q.defer();
+			var user;
+			var fields;
+			console.log(event)
 
-    if (event.userIdentity.type === 'IAMUser') {
-      user = event.userIdentity.userName;
-    } else if (event.userIdentity.type === 'Root') {
-      user = event.userIdentity.userName || 'Root';
-    } else if (event.userIdentity.type === 'AssumedRole') {
-      user = event.userIdentity.arn.split(':')[5].replace('assumed-role/', '');
-    } else {
-      user = event.userIdentity.arn.split(':')[5];
-    }
+			if (event.userIdentity.type === 'IAMUser') {
+				user = event.userIdentity.userName;
+			} else if (event.userIdentity.type === 'Root') {
+				user = event.userIdentity.userName || 'Root';
+			} else if (event.userIdentity.type === 'AssumedRole') {
+				user = event.userIdentity.arn.split(':')[5].replace('assumed-role/', '');
+			} else {
+				user = event.userIdentity.arn.split(':')[5];
+			}
 
-    fields = [
-      {
-        title: 'EventID',
-        value: event.eventID,
-        short: true
-      },
-      {
-        title: 'Timestamp',
-        value: new Date(event.eventTime).toString(),
-        short: true
-      },
-      {
-        title: 'User',
-        value: user,
-        short: true
-      },
-      {
-        title: 'Source',
-        value: event.eventSource,
-        short: true
-      },
-      {
-        title: 'Name',
-        value: event.eventName,
-        short: true
-      }
-    ]
+			fields = [
+				{
+					title: 'EventID',
+					value: event.eventID,
+					short: true
+				},
+				{
+					title: 'Timestamp',
+					value: new Date(event.eventTime).toString(),
+					short: true
+				},
+				{
+					title: 'User',
+					value: user,
+					short: true
+				},
+				{
+					title: 'Source',
+					value: event.eventSource,
+					short: true
+				},
+				{
+					title: 'Name',
+					value: event.eventName,
+					short: true
+				},
+				{
+					title: 'Region',
+					value: event.awsRegion,
+					short: true
+				}
 
-    if (Fields[event.eventName]) {
-      fields = _.flatten(_.concat(fields, Fields[event.eventName](event)));
-    }
+			]
 
-    return {
-      color: '#789C45',
-      fields: fields
-    }
-  });
+			if (Fields[event.eventName]) {
+				Fields[event.eventName](event)
+					.then(function (arr) {
+						fields = _.flatten(_.concat(fields, arr));
+						deferred.resolve({
+							color: '#789C45',
+							fields: fields
+						})
+					}).catch(function (error) {
+					deferred.reject(error)
+				})
 
-  slack.setWebhook(process.env.SLACK_WEBHOOK);
-  slack.webhook({
-    text: '<https://us-east-1.console.aws.amazon.com/cloudtrail/home|Open CloudTrail Console>',
-    attachments: _.flatten(attachments)
-  }, function(error, response) {
-    if (!error) {
-      deferred.resolve(response);
-    } else {
-      deferred.reject(error);
-    }
-  });
+			} else {
+				deferred.resolve({
+					color: '#789C45',
+					fields: fields
+				})
+			}
 
-  return deferred.promise;
+			return deferred.promise
+		}
+	)
+	var deferred = Q.defer();
+	Q.all(attachments).then(function (results) {
+		var slack = new Slack();
+		slack.setWebhook(process.env.SLACK_WEBHOOK);
+		slack.webhook({
+			text: '<https://us-east-1.console.aws.amazon.com/cloudtrail/home|Open CloudTrail Console>',
+			attachments: _.flatten(results)
+		}, function (error, response) {
+			if (!error) {
+				deferred.resolve(response);
+			} else {
+				deferred.reject(error);
+			}
+		});
+	}).catch(function (error) { deferred.reject(error);})
+
+	return deferred.promise;
 }
 
-exports.handler = function(input, context) {
-  Dotenv.config();
-
-  read(input.awslogs.data)
-    .then(filter)
-    .then(announce)
-    .then(context.succeed)
-    .catch(context.done);
+exports.handler = function (input, context, callback) {
+	Dotenv.config();
+	console.log(input)
+	read(input.awslogs.data)
+		.then(filter)
+		.then(announce)
+		.then(function (result) {callback(null, result)})
+		.catch(function (error) {callback(error)})
+		.done();
 }
